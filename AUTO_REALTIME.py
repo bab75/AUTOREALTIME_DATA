cc6b86b685ad87c2da925bfac29367b96f11a9b3
bab75 <dealbab75@gmail.com>
2025-07-08 15:49:07 -0400

    Update AUTO_REALTIME.py

--- AUTO_REALTIME.py
+++ AUTO_REALTIME.py
@@ -2,6 +2,7 @@ import streamlit as st
 import yfinance as yf
 import pandas as pd
 import plotly.graph_objects as go
+import time  # Added missing import
 from datetime import datetime
 import pytz
 
